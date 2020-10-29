3cf3946fefbb6cf729171ab876c67a9c921d9538
Lars Wiegers <larswiegers@live.nl>
2020-10-29 19:49:31 +0100

    Looks a lot better, it now works based on an event

--- main.py
+++ main.py
@@ -1,17 +1,34 @@
-import os
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
 
+import os
+import time
 downloadsFolder = r'C:\Users\Windows\Downloads'
 
-for file in os.listdir(downloadsFolder):
-    parts = file.split('.')
-    length = len(parts)
+class DownloadsFolderListener(FileSystemEventHandler):
+    def on_modified(self, event):
+        for file in os.listdir(downloadsFolder):
+            parts = file.split('.')
+            length = len(parts)
 
-    directoryPath = downloadsFolder + "\\" + parts[length - 1]
+            directoryPath = downloadsFolder + "\\" + parts[length - 1]
 
-    if not os.path.exists(directoryPath):
-        os.makedirs(directoryPath)
+            if not os.path.exists(directoryPath):
+                os.makedirs(directoryPath)
 
-    try:
-        os.rename(downloadsFolder + "\\" + file, directoryPath + "\\" + file)
-    except PermissionError as error:
-        print(str(error))
+            try:
+                os.rename(downloadsFolder + "\\" + file, directoryPath + "\\" + file)
+            except PermissionError as error:
+                print()
+
+
+event_handler = DownloadsFolderListener()
+observer = Observer()
+observer.schedule(event_handler, downloadsFolder, recursive=True)
+observer.start()
+try:
+    while True:
+        time.sleep(10)
+except KeyboardInterrupt:
+    observer.stop()
+observer.join()
